e0607e66927eacc38ae4845b90b6086a4d817211
zhiqiang lu <281747606@qq.com>
2020-07-16 13:44:34 +0800

    ...

--- src/main/java/org/zhiqiang/lu/maven/plugins/mybatis/generator/plugins/GeneratorDateTimeFormat.java
+++ src/main/java/org/zhiqiang/lu/maven/plugins/mybatis/generator/plugins/GeneratorDateTimeFormat.java
@@ -8,7 +8,6 @@ import org.mybatis.generator.api.dom.java.TopLevelClass;
 
 import java.util.List;
 
-
 public class GeneratorDateTimeFormat extends PluginAdapter {
     public GeneratorDateTimeFormat() {
     }
@@ -17,14 +16,15 @@ public class GeneratorDateTimeFormat extends PluginAdapter {
         return true;
     }
 
-    @Override
-    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
-        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
-    }
+    // @Override
+    // public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+    // IntrospectedTable introspectedTable) {
+    // return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    // }
 
     @Override
-    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+            IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         String date = this.properties.getProperty("dateFormat");
         String time = this.properties.getProperty("timeFormat");
         if (null == date) {
@@ -35,9 +35,12 @@ public class GeneratorDateTimeFormat extends PluginAdapter {
         }
         if (introspectedColumn.getJdbcType() == 93) {
             field.addAnnotation("@JsonFormat(timezone = \"GMT+8\", pattern = \"" + time + "\")");
+            topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
         } else if (introspectedColumn.getJdbcType() == 91) {
             field.addAnnotation("@JsonFormat(timezone = \"GMT+8\", pattern = \"" + date + "\")");
+            topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
         }
+
         return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
     }
 }
