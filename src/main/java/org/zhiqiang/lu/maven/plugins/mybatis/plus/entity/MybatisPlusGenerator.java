6c784e10df9bbea83890a39a126ea159cdc74332
Q <281747606@qq.com>
2021-05-29 19:56:48 +0800

    ...

---
+++ src/main/java/org/zhiqiang/lu/maven/plugins/mybatis/plus/entity/MybatisPlusGenerator.java
@@ -0,0 +1,13 @@
+package org.zhiqiang.lu.maven.plugins.mybatis.plus.entity;
+
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+
+public class MybatisPlusGenerator {
+  private GlobalConfig globalConfig;
+  private DataSourceConfig dataSourceConfig;
+  private PackageConfig packageConfig;
+  private StrategyConfig strategyConfig;
+}
